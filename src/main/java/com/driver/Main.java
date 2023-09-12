5ad06bcbfaeb27297a3014fefb631828e11f5e74
=vamsi <=>
2023-09-12 12:36:08 +0530

    second commite

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,14 +8,3 @@ public class Main {
   }
 }
 
-    class A{
-    public String meth(){
-    return "Invoking method from class A";
-        }
-    }
-
-    class B extends A{
-   public String meth(){
-     return "Method is overridden in Extendend class B";
-   }
-    }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,20 @@
+//package com.driver.test;
+//
+//import static org.junit.Assert.*;
+//import org.junit.jupiter.api.Test;
+//
+//public class TestCases {
+//
+//}
+
 package com.driver.test;
 
-import static org.junit.Assert.*;
-import org.junit.jupiter.api.Test;
+import com.driver.B;
 
 public class TestCases {
-    
+    public static void main(String[] args) {
+        B objectB = new B();
+        String ans = objectB.meth();
+        System.out.println(ans);
+    }
 }
