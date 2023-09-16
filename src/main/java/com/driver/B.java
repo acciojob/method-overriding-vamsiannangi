0e60e9f018952da1c91c97ed46d56c1a426c8654
vamsi <117896369+vamsiannangi@users.noreply.github.com>
2023-09-16 16:02:12 +0530

    Delete src/main/java/com/driver/B.java

--- src/main/java/com/driver/B.java
+++
@@ -1,8 +0,0 @@
-package com.driver;
-
- public static class B extends A {
-        public  String meth()
-        {
-            return "Method is overridden in Extendend class B";
-        }
-    }
