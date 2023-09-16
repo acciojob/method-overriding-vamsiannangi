31476676b30c297fd2fa3a656bbd2e1d3e27673d
vamsi <117896369+vamsiannangi@users.noreply.github.com>
2023-09-16 15:40:08 +0530

    Update A.java

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,7 +1,9 @@
 package com.driver;
 
-public class A{
-    public String meth(){
-        return "Invoking method from class A";
+public static class A {
+
+        public  String meth()
+        {
+            return "Invoking method from class A";
+        }
     }
-}
