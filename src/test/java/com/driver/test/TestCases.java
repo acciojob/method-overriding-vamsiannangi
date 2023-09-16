826b8620971c930bcbe050f3c10326adf54976d3
vamsi <117896369+vamsiannangi@users.noreply.github.com>
2023-09-16 16:03:19 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,20 +1,10 @@
-//package com.driver.test;
-//
-//import static org.junit.Assert.*;
-//import org.junit.jupiter.api.Test;
-//
-//public class TestCases {
-//
-//}
-
 package com.driver.test;
 
-import com.driver.B;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    public static void main(String[] args) {
-        B objectB = new B();
-        String ans = objectB.meth();
-        System.out.println(ans);
-    }
+
 }
+
+
