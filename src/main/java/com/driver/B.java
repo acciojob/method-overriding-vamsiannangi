ffd5d47a43f14457b088054bceb8b50a57b48d87
vamsi <117896369+vamsiannangi@users.noreply.github.com>
2023-09-16 15:40:34 +0530

    Update B.java

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,7 +1,8 @@
 package com.driver;
 
-public class B extends A {
-    public String meth() {
-        return "Method is overridden in Extendend class B";
+ public static class B extends A {
+        public  String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
     }
-}
