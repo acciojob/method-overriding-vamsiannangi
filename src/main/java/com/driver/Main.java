91fe583f67e10b67ccd1cd548728cb91f5495c98
vamsi <117896369+vamsiannangi@users.noreply.github.com>
2023-09-16 15:39:32 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,12 @@
 package com.driver;
 
 public class Main {
-  public static void main(String[] args){
-      B objectB=new B();
-      String ans=objectB.meth();
-      System.out.println(ans);
-  }
+  public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+
+        B obj1 = new B();
+        obj1.meth();
+    }
 }
 
